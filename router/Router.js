ea04943fa1f1144f87421dc0eda2e1a1c1b2bc9a
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-02-02 20:42:22 -0300

    Load initial route

--- router/Router.js
+++ router/Router.js
@@ -1,5 +1,13 @@
 class Router {
     constructor(routes) {
         this.routes = routes;
+        this._loadInitialRoute();
+    }
+
+    _loadInitialRoute() {
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
+
+        this.loadRoute(...pathSegs);
     }
 }
