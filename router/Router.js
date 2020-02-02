b94d95088cccbf88e3c97c54492ae694e0f1b49a
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-02-02 20:42:42 -0300

    Match URL route

--- router/Router.js
+++ router/Router.js
@@ -4,6 +4,20 @@ class Router {
         this._loadInitialRoute();
     }
 
+    _matchUrlToRoute(urlSegs) {
+        const matchedRoutes = this.routes.find(route => {
+            const routePathSegs = route.path.split('/').slice(1);
+
+            if (routePathSegs.length !== urlSegs.length) {
+                return false;
+            }
+
+            return routePathSegs.every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+        });
+
+        return matchedRoutes;
+    }
+
     _loadInitialRoute() {
         const pathNameSplit = window.location.pathname.split('/');
         const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
